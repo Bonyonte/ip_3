09033c2d9f26679ad0e00deab07e8b5e4bc56205
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-05-15 21:55:09 +0300

    add radio type input

---
+++ index.js
@@ -0,0 +1,24 @@
+function check(){
+    var c=0;
+    var a1=document.quiz.answer1.value;
+    var a2=document.quiz.answer2.value;
+    var a3=document.quiz.answer3.value;
+    var a4=document.quiz.answer4.value;
+    var a5=document.quiz.answer5.value;
+    var a6=document.quiz.answer6.value;
+    var a7=document.quiz.answer7.value;
+    var a8=document.quiz.answer8.value;
+    var a9=document.quiz.answer9.value;
+    var a10=document.quiz.answer10.value;
+    if (a1=="White") {c++}
+    if (a2=="Art set up") {c++}
+    if (a3=="To point out the flaws in the design") {c++}
+    if (a4=="PhotoSHOP") {c++}
+    if (a5=="True") {c++}
+    if (a6=="False") {c++}
+    if (a7=="True") {c++}
+    if (a8=="Violet") {c++}
+    if (a9=="Illustrator") {c++}
+    if (a10=="Violet") {c++}
+    document.write (c);
+}
